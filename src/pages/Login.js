04fce04d30b62dba71fef34e388203d39e75fba4
Lemuel Francisco <lemuelrfrancisco@gmail.com>
2024-05-04 10:21:25 +0800

    activity

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,10 @@
-import { createContext, useContext, useState } from 'react';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+} from 'react';
 
 const ThemeContext = createContext(null);
 
@@ -22,10 +28,45 @@ export default function Login() {
 }
 
 function Form({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    setUsername(usernameRef.current.value);
+    setPassword(passwordRef.current.value);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+    setPassword('');
+  };
   return (
-    <Panel title='Welcome'>
-      <Button>Sign up</Button>
-      <Button>Log in</Button>
+    <Panel title={`Welcome ${username}`}>
+      {!isLoggedIn ? (
+        <>
+          <div style={{ display: 'block', marginBottom: 5 }}>
+            <span>Username:</span>
+            <input type='text' ref={usernameRef} />
+          </div>
+          <div style={{ display: 'block', marginBottom: 5 }}>
+            <span>Password:</span>
+            <input type='password' ref={passwordRef} />
+          </div>
+          <Button>Sign up</Button>
+          <Button onClick={handleLogin}>Log in</Button>
+        </>
+      ) : (
+        <>
+          <p>Password:{password}</p>
+          <Button onClick={handleLogout}>Log out</Button>
+        </>
+      )}
     </Panel>
   );
 }
@@ -41,9 +82,13 @@ function Panel({ title, children }) {
   );
 }
 
-function Button({ children }) {
+function Button({ children, onClick }) {
   const theme = useContext(ThemeContext);
   const className = 'button-' + theme;
-  return <button className={className}>{children}</button>;
+  return (
+    <button className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
